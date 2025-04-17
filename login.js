5b1494958bab28cd45b920f9ba3bb00affc6b4d5
cwyaaa <wuyangcui69@gmail.com>
2025-04-17 12:37:10 +0800

    Add files via upload

--- login.js
+++ login.js
@@ -1,39 +1,16 @@
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-app.js";
 import { getAuth, onAuthStateChanged, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-auth.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCcsz9Lg3KmgvD_SXUdy4V5x7aCQtSdI-s",
-  authDomain: "webs-d0804.firebaseapp.com",
-  projectId: "webs-d0804",
-  storageBucket: "webs-d0804.firebasestorage.app",
-  messagingSenderId: "195195304680",
-  appId: "1:195195304680:web:01c68159689678c7f4275d"
-};
+import { firebaseConfig } from './config.js';
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
-let currentUser = null;
-
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    currentUser = user;
-    document.getElementById("title").innerText = "欢迎回来：" + user.email;
-    document.getElementById("login-section").classList.add("hidden");
-    document.getElementById("signup-section").classList.add("hidden");
-    document.getElementById("app").classList.remove("hidden");
-  } else {
-    document.getElementById("login-section").classList.remove("hidden");
-    document.getElementById("signup-section").classList.add("hidden");
-    document.getElementById("app").classList.add("hidden");
-  }
-});
 
 window.login = async function () {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
   try {
     await signInWithEmailAndPassword(auth, email, password);
+    window.location.href = "bookmarks.html";
   } catch (error) {
     alert(error.message);
   }
@@ -56,16 +33,12 @@ window.signup = async function () {
   }
 }
 
-window.logout = async function () {
-  await signOut(auth);
-}
-
-window.showSignup = function () {
+function showSignup() {
   document.getElementById("login-section").classList.add("hidden");
   document.getElementById("signup-section").classList.remove("hidden");
 }
 
-window.showLogin = function () {
+function showLogin() {
   document.getElementById("signup-section").classList.add("hidden");
   document.getElementById("login-section").classList.remove("hidden");
 }
